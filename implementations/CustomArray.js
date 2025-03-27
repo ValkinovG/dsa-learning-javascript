8dff7b41631f74dcd7d75c3a5b9c59f895694df0
Genko Valkinov <117573753+ValkinovG@users.noreply.github.com>
2025-03-27 19:31:08 +0200

    Added get method with bounds checking to CustomArray

--- implementations/CustomArray.js
+++ implementations/CustomArray.js
@@ -13,6 +13,14 @@ class CustomArray {
         this.#length++;
     }
 
+    get(index) {
+        if (index < 0 || index >= this.#length) {
+            throw new Error('Index is out of bounds.');
+        }
+
+        return this.#data[index];
+    }
+
     toString() {
         if (this.#length < 1) {
             return 'Array is empty.'
