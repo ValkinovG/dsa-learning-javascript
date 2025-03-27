5bca7ea489933feea93e4ede678a6b3e45542374
Genko Valkinov <117573753+ValkinovG@users.noreply.github.com>
2025-03-27 19:29:09 +0200

    Added toString method to CustomArray

--- implementations/CustomArray.js
+++ implementations/CustomArray.js
@@ -8,6 +8,19 @@ class CustomArray {
         this.#data = {};
     }
 
+    toString() {
+        if (this.#length < 1) {
+            return 'Array is empty.'
+        }
+
+        let returnString = `index 0: ${this.#data[0]}`;
+        for (let i = 1; i < this.#length; i++) {
+            returnString += `, index ${i}: ${this.#data[i]}`
+        }
+
+        return returnString;
+    }
+
     get length() {
         return this.#length;
     }
