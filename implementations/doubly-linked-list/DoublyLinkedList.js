758230487f883ba521cc409719625f42cf754cae
Genko Valkinov <117573753+ValkinovG@users.noreply.github.com>
2025-03-29 17:13:49 +0200

    Added push() method to DoublyLinkedList

--- implementations/doubly-linked-list/DoublyLinkedList.js
+++ implementations/doubly-linked-list/DoublyLinkedList.js
@@ -11,6 +11,21 @@ class DoublyLinkedList {
         this.#length = 0;
     }
 
+    push(value) {
+        const newDoublyNode = new DoublyNode(value);
+
+        if (this.#length === 0) {
+            this.#head = newDoublyNode;
+            this.#tail = newDoublyNode;
+        } else {
+            newDoublyNode.prev = this.#tail;
+            this.#tail.next = newDoublyNode;
+            this.#tail = newDoublyNode;
+        }
+
+        this.#length++;
+    }
+
     get head() {
         return this.#head;
     }
