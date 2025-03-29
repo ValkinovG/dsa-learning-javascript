8ee8c1fd0116f15af766e7127d3fa2a46ca9d7a3
Genko Valkinov <117573753+ValkinovG@users.noreply.github.com>
2025-03-29 16:59:11 +0200

    Added DoublyLinkedList class with fields and constructor

---
+++ implementations/doubly-linked-list/DoublyLinkedList.js
@@ -0,0 +1,13 @@
+import { DoublyNode } from "./DoublyNode.js";
+
+class DoublyLinkedList {
+    #head;
+    #tail;
+    #length;
+
+    constructor() {
+        this.#head = null;
+        this.#tail = null;
+        this.#length = 0;
+    }
+}
