2d144bf09df56b3e2169fb41299ae9da43c7c30f
Genko Valkinov <117573753+ValkinovG@users.noreply.github.com>
2025-03-29 16:56:57 +0200

    Added DoublyNode class for a DoublyLinkedList

---
+++ implementations/doubly-linked-list/DoublyNode.js
@@ -0,0 +1,8 @@
+// Node class for doubly linked list
+export class DoublyNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
